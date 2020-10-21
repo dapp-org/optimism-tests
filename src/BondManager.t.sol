1460f3d12238fd63c0c99fc902c340bd3daa52c0
Martin Lundfall <martin.lundfall@protonmail.com>
2020-10-21 15:11:21 +0200

    bump

--- src/BondManager.t.sol
+++ src/BondManager.t.sol
@@ -1,4 +1,5 @@
 import { OVM_BondManager } from "../contracts-v2/contracts/optimistic-ethereum/OVM/verification/OVM_BondManager.sol";
+import {DSTest} from "ds-test/test.sol";
 
 contract BondManagerTest is DSTest {
     function setUp() public {
