63497a57f5e6a9cbf1db597ca426846dd7d9a842
David Terry <me@xwvvvvwx.com>
2020-11-11 13:23:10 +0100

    correct solc version for linux

--- src/BondManager.t.sol
+++ src/BondManager.t.sol
@@ -8,13 +8,16 @@ contract BondManagerTest is DSTest {
     Lib_AddressManager manager;
     Lib_AddressResolver resolver;
     OVM_BondManager mgr;
-    
+
     function setUp() public {
         manager = new Lib_AddressManager();
         // resolver = new Lib_AddressResolver(address(manager));
         mgr = new OVM_BondManager(ERC20(address(0x00)),address(manager));
     }
     function test_a() public {
+        assertEq(address(mgr.token()), address(0x00));
+    }
+    function prove_a() public {
         assertEq(address(mgr.token()), address(0x00));
     }
 }
