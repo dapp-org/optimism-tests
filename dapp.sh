0ae6af8d5549a263afae0c0624c94a6b8cf00a46
David Terry <me@xwvvvvwx.com>
2020-11-23 19:24:32 +0100

    rm dapp.sh

--- dapp.sh
+++
@@ -1,5 +0,0 @@
-if [ `uname` == Darwin ]; then
-    dapp --use ./solc-macos-0.7.4 $*
-else
-    dapp --use ./solc-linux-0.7.4 $*
-fi
--- shell.nix
+++ shell.nix
@@ -4,4 +4,7 @@ let
 in
   pkgs.mkShell {
     buildInputs = with pkgs; [ dapp seth hevm niv ethsign ];
+    DAPP_SOLC = if pkgs.stdenv.isDarwin
+                then (toString ./solc-macos-0.7.4)
+                else (toString ./solc-linux-0.7.4);
   }
